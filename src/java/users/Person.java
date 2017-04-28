db419c0f263e80e039abbc094a1190c07aeea290
Zhe <zhe.lin@stonybrook.edu>
2017-04-28 02:10:51 -0400

    created the person object for database

---
+++ src/java/users/Person.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package users;
+import java.util.Date;
+import java.io.*;
+
+/**
+ *
+ * @author ZheLin
+ */
+public class Person implements Serializable{
+    // Properties.
+    private int id; //It's account number for customers and ssn for employees
+    private String FName;
+    private String LName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phone;
+    
+
+
+    // Getters.
+    public int getId() { return id; }
+    public String getFName() { return FName; }
+    public String getLName() { return LName; }
+    public String getAddress() { return address; }
+    public String getCity() { return city; }
+    public String getState() { return state; }
+    public int getZip() { return zip; }
+    public long getPhone() { return phone; }
+   
+
+    // Setters.
+    public void setId(int id) { this.id = id; }
+    public void setFName(String FName) { this.FName = FName; }
+    public void setLName(String LName) { this.LName = LName; }
+    public void setAddress(String address) { this.address = address; }
+    public void setCity(String city) { this.city = city; }
+    public void setState(String state) { this.state = state; }
+    public void setZip(int zip) { this.zip = zip; }
+    public void setPhone(long phone) { this.phone = phone; }
+    
+}
