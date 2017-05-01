7d333896bc0bd1b37a9ae0541ac4b4ac2cc33aea
Zhe <zhe.lin@stonybrook.edu>
2017-05-01 01:56:47 -0400

    added Movie object

---
+++ src/java/movieData/Movie.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieData;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ZheLin
+ */
+public class Movie implements Serializable{
+    // properties
+    private int id;
+    private String title;
+    private String genre;
+    private double fee;
+    private int copies;
+    private int rating;
+    
+    // getters
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getGenre() { return genre; }
+    public double getFee() { return fee; }
+    public int getCopies() { return copies; }
+    public int getRating() { return rating; }
+    
+    // setters
+    public void setId(int id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public void setFee(double fee) { this.fee = fee; }
+    public void setCopies(int copies) { this.copies = copies; }
+    public void setRating(int rating) { this.rating = rating; }
+    
+}
