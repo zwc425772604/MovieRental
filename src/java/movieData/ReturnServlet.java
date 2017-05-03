49364082e08a72d158ef44a01d76ff68deb3c378
Zhe <zhe.lin@stonybrook.edu>
2017-05-03 01:20:49 -0400

    return is working well

--- src/java/movieData/ReturnServlet.java
+++ src/java/movieData/ReturnServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,23 +22,35 @@ import javax.servlet.http.HttpSession;
  * @author ZheLin
  */
 public class ReturnServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {			 
+        ServletContext context= getServletContext();
+        RequestDispatcher rd= context.getRequestDispatcher("/CustomerInfoServlet");
+        rd.forward(request, response);
+    }
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         System.out.println("!!!!!!!!!!");
         HttpSession session=request.getSession();  
-	
-        String cusId = ""+session.getAttribute("login");
 	          
-	int returnIndex = Integer.parseInt(request.getParameter("returnindex"));
         ArrayList<Movie> rentList = (ArrayList<Movie>) session.getAttribute("rentList");
+        long orderId = Long.parseLong(request.getParameter("orderId"));
         String mysJDBCDriver = "com.mysql.jdbc.Driver"; 
         String mysURL ="jdbc:mysql://127.0.0.1:3306/cse305";
         String mysUserID = "root"; 
         String mysPassword = "1234";
         
         // First delete it in the list
-        Movie movie = rentList.remove(returnIndex);
+        int i = 0;
+        for(; i < rentList.size(); i++) {
+            Movie movie = rentList.get(i);
+            if (movie.getOrderId() == orderId){
+                break;
+            }
+        }
+        
+        Movie movie = rentList.remove(i);
         
         // then go to database
         java.sql.Connection conn=null;
@@ -51,7 +64,7 @@ public class ReturnServlet extends HttpServlet {
             conn=java.sql.DriverManager.getConnection(mysURL,sysprops);
 
             java.sql.CallableStatement stmt = conn.prepareCall("{call SetReturned(?)}");
-            stmt.setLong(1, movie.getOrderId());
+            stmt.setLong(1, orderId);
             stmt.execute();
             stmt.close();
             
