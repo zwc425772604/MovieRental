8d7de49843b227c8e90e4052a8cf8d445c8ae13a
SeanPesce <pesce.sean@gmail.com>
2017-05-04 00:32:32 -0400

    Update make.bat with correct classpath

--- make.bat
+++ make.bat
@@ -10,18 +10,19 @@ IF "%1"=="all" (
 	echo:
 	REM Remove old build files:
 	del /f /s /q "%~dp0build" 2> nul > nul
+	rmdir "%~dp0build/classes" /s /q 2> nul > nul
 	rmdir "%~dp0build" /s /q 2> nul > nul
 
 	echo Creating build directory...
 	REM Create build directory:
-	echo mkdir build
+	echo mkdir build\classes
 	echo:
-	mkdir "%~dp0build" 2> nul
+	mkdir "%~dp0build\classes" 2> nul
 
 	echo Compiling Java...
 	REM Compile the .java files:
 	echo javac -d build -classpath .;lib/*.jar src/java/users/*.java src/java/movieData/*.java
-	javac -d "%~dp0build" -classpath .;"%~dp0lib/apache-tomcat-6.0.53/servlet-api.jar";"%~dp0lib/MySQLDriver/mysql-connector-java-5.1.23-bin.jar";"%~dp0lib/javaee-endorsed-api-6.0/javaee-doc-api.jar";"%~dp0lib/javaee-endorsed-api-6.0/javax.annotation.jar";"%~dp0lib/javaee-endorsed-api-6.0/jaxb-api-osgi.jar";"%~dp0lib/javaee-endorsed-api-6.0/webservices-api-osgi.jar";"%~dp0lib/CopyLibs/org-netbeans-modules-java-j2seproject-copylibstask.jar" "%~dp0src/java/users/"*.java "%~dp0src/java/movieData/"*.java
+	javac -d "%~dp0build\classes" -classpath .;"%~dp0lib/apache-tomcat-6.0.53/servlet-api.jar";"%~dp0lib/MySQLDriver/mysql-connector-java-5.1.23-bin.jar";"%~dp0lib/javaee-endorsed-api-6.0/javaee-doc-api.jar";"%~dp0lib/javaee-endorsed-api-6.0/javax.annotation.jar";"%~dp0lib/javaee-endorsed-api-6.0/jaxb-api-osgi.jar";"%~dp0lib/javaee-endorsed-api-6.0/webservices-api-osgi.jar";"%~dp0lib/CopyLibs/org-netbeans-modules-java-j2seproject-copylibstask.jar" "%~dp0src/java/users/"*.java "%~dp0src/java/movieData/"*.java
 	
 	goto :done
 )
